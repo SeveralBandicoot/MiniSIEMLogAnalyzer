99ff1b1bdb9ad2f69c394d7c8742c3614222d53d
technicalace <arguello.aj256@gmail.com>
2025-12-22 10:42:49 -0600

    extraction and assignment of message successfully running

--- logParser.cpp
+++ logParser.cpp
@@ -12,15 +12,20 @@ void logParser(string& rawData) {
 
     vector<string> tokens; // vector of string to save tokens
     vector<string> timestamp; 
+    vector<string> message; 
 
     stringstream ss(rawData);
     stringstream dt; // date and time
+    stringstream msg; // message
 
     string intermediate; 
     //----tokenizing using whitespace----
     while (getline(ss, intermediate, ' ')) { 
         tokens.push_back(intermediate);
     }
+
+    // TIMESTAMP 
+
     //----concatenating date and time----
     for (size_t i = 0; i < 2; i++) { // push back the data and time from tokens vector to timestamp vector
         timestamp.push_back(tokens[i]);
@@ -37,11 +42,32 @@ void logParser(string& rawData) {
     
     entry.timestamp = timeStamp; // assign concatenated timestamp to the log entries timestamp defined in LogEntry.h
 
-    //----extract and assign level----
-    //cout << ss.str() << endl; // print entire raw data
-    string entireLevelStream = tokens[5];
-    entireLevelStream = entireLevelStream.substr(6);
-    entry.level = entireLevelStream; 
+    // LEVEL
 
-    
+    //----extract and assign level----
+    string levelStream = tokens[5];
+    levelStream = levelStream.substr(6);
+    entry.level = levelStream; 
+
+    // MESSAGE 
+
+    //----concatenating message----
+    for (size_t i = 21; i < tokens.size(); i++) {
+        message.push_back(tokens[i]);
+    }
+
+    for (string& word : message) {
+        msg << word << " "; // append each word
+    }
+    //----extract message from message vector----
+    string msgStream = msg.str();
+    msgStream = msgStream.substr(4);
+    entry.message = msgStream;
+
+    // REQUEST ID
+
+    /*for (string word : tokens) { // prints out tokens vector line by line (element by element)
+        cout << word << endl;
+    }*/
+
 }
